5d87ceb391416d3fc4f55eb75282fdf9089939d2
Daniel Solorzano <dsolo.prof@gmail.com>
2024-08-26 14:21:42 -0500

    Cleaned up ContentPage component and CSS.

--- src/components/ContentPage/ContentPage.tsx
+++ src/components/ContentPage/ContentPage.tsx
@@ -2,15 +2,18 @@ import RandomChoice from "../RandomChoice/RandomChoice";
 import SearchChoice from "../SearchChoice/SearchChoice";
 import { useState } from "react";
 
+//Possible focus sections.
 export enum FOCUSTYPE {
     LEFT = "left",
     RIGHT = "right",
     NONE = "none",
 }
 
+//Used for determining fade-in and fade-out speed.
 export const ANIMATION_DELAY_SYNC: number = 200;
 
 export default function ContentPage() {
+    //Determine which side has the main focus.
     let [focusSide, setFocusSide] = useState<FOCUSTYPE>(FOCUSTYPE.NONE);
 
     return (
